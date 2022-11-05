b63701b3b3cd2d9d8833c0f452ad0a0a50b5d6b2
Rupal Majmudar <rupal.majmudar@bob.ch>
2022-11-05 12:04:30 +0100

    works!

---
+++ frontend/src/App.js
@@ -0,0 +1,68 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  query,
+  collection,
+  onSnapshot,
+  getFirestore,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfYOErx8SA0c8OcI4rjlr-U7FIytyoYAc",
+  authDomain: "rm-moralis-on-firebase.firebaseapp.com",
+  projectId: "rm-moralis-on-firebase",
+  storageBucket: "rm-moralis-on-firebase.appspot.com",
+  messagingSenderId: "500023830064",
+  appId: "1:500023830064:web:98540da6532a6f0f5aaff5",
+  measurementId: "G-TNV0MES6X0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+function App() {
+  const [txs, setTxs] = useState(null);
+
+  useEffect(() => {
+    console.log("start");
+    const q = query(collection(db, "moralis/txs/Rm_mm_polygon_testnet"));
+    console.log("query has been setup");
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const tempTxs = [];
+      querySnapshot.forEach((doc) => {
+        tempTxs.push(doc.data());
+      });
+      setTxs(tempTxs);
+    });
+
+
+    //Stop realtime updates:
+    //unsubscribe();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>🔥 Firebase Moralis Streams Extension 🔥</p>
+            <table className="table">
+              <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Amount</th>
+              </tr>
+              {txs?.map((e,i)=>{
+                return(
+                  <tr key={i}>
+                    <td>{e.fromAddress}</td>
+                    <td>{e.toAddress}</td>
+                    <td>{e.value / 1E18} Matic</td>
+                  </tr>
+                )
+              })}
+            </table>
+      </header>
+    </div>
+  );
+}
+export default App;
